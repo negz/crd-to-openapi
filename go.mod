c498b0ba2a91fed43d6d2789caba4a51208f7cb2
Nic Cope <nicc@rk0n.org>
2024-09-27 18:14:48 -0700

    Even simpler
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- go.mod
+++ go.mod
@@ -7,9 +7,8 @@ toolchain go1.22.7
 require (
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/pflag v1.0.5
-	github.com/thoas/go-funk v0.9.3
-	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/apiextensions-apiserver v0.31.1
+	sigs.k8s.io/yaml v1.4.0
 )
 
 require (
@@ -74,6 +73,7 @@ require (
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.31.1 // indirect
 	k8s.io/apimachinery v0.31.1 // indirect
 	k8s.io/apiserver v0.31.1 // indirect
@@ -85,5 +85,4 @@ require (
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.30.3 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
-	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
 	"os"
+	"regexp"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apiextensions-apiserver/pkg/controller/openapi/builder"
+	"sigs.k8s.io/yaml"
 )
 
 func main() {
@@ -33,4 +43,92 @@ func NewRootCommand(flags *pflag.FlagSet) *cobra.Command {
 	}
 
 	return cmd
+}
+
+func Convert(version string, v2 bool) error {
+	reader := bufio.NewReader(os.Stdin)
+	src, err := io.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+
+	crd, err := ReadCRD(src)
+	if err != nil {
+		return err
+	}
+
+	if len(crd.Spec.Versions) == 0 {
+		return fmt.Errorf("CRD doesn't have any version")
+	}
+
+	if version == "" {
+		var versions SortableVersions
+		for _, crdVersion := range crd.Spec.Versions {
+			versions = append(versions, crdVersion.Name)
+		}
+		sort.Sort(versions)
+		version = versions[len(versions)-1]
+	} else {
+		exists := map[string]bool{}
+		for _, v := range crd.Spec.Versions {
+			exists[v.Name] = true
+		}
+		if !exists[version] {
+			return fmt.Errorf("CRD doesn't have version %s, but has %v", version, exists)
+		}
+	}
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if v2 {
+		output, err := builder.BuildOpenAPIV2(crd, version, builder.Options{V2: true})
+		if err != nil {
+			return err
+		}
+		return encoder.Encode(output)
+	}
+
+	output, err := builder.BuildOpenAPIV3(crd, version, builder.Options{})
+	if err != nil {
+		return err
+	}
+	return encoder.Encode(output)
+}
+
+func ReadCRD(src []byte) (*apiextensionsv1.CustomResourceDefinition, error) {
+	var crd apiextensionsv1.CustomResourceDefinition
+
+	var err error
+	if err = json.Unmarshal(src, &crd); err != nil {
+		err = yaml.Unmarshal(src, &crd)
+	}
+
+	return &crd, err
+}
+
+type SortableVersions []string
+
+func (a SortableVersions) Len() int      { return len(a) }
+func (a SortableVersions) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a SortableVersions) Less(i, j int) bool {
+	vi, vj := strings.TrimLeft(a[i], "v"), strings.TrimLeft(a[j], "v")
+	major := regexp.MustCompile("^[0-9]+")
+	viMajor, vjMajor := major.FindString(vi), major.FindString(vj)
+	viRemaining, vjRemaining := strings.TrimLeft(vi, viMajor), strings.TrimLeft(vj, vjMajor)
+	switch {
+	case len(viRemaining) == 0 && len(vjRemaining) == 0:
+		return viMajor < vjMajor
+	case len(viRemaining) == 0 && len(vjRemaining) != 0:
+		// stable version is greater than unstable version
+		return false
+	case len(viRemaining) != 0 && len(vjRemaining) == 0:
+		// stable version is greater than unstable version
+		return true
+	}
+	// neither are stable versions
+	if viMajor != vjMajor {
+		return viMajor < vjMajor
+	}
+	// assuming at most we have one alpha or one beta version, so if vi contains "alpha", it's the lesser one.
+	return strings.Contains(viRemaining, "alpha")
 }
